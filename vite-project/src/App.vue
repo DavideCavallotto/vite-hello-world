471d13700f5acbef8762c31ce7d3a64c955a3ffc
DavideCavallotto <davidecavallotto92@gmail.com>
2023-11-03 16:25:07 +0100

    ADD image from components

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,26 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHero from './components/AppHero.vue'
+
+export default {
+  data() {
+    return {
+      message: 'La mia prima app con Vite!'
+    }
+  },
+  components: {
+    AppHero
+  },
+  methods: {
+    imClick() {
+      console.log('Ho Cliccato')
+    }
+
+  }
+}
 </script>
 
 <template>
- <div>
-  <h1>
-    La mia prima app con Vite!
-  </h1>
+ <div @click="imClick()">  
+  {{ message }} 
  </div>
+  <AppHero/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ vite-project/src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      text: 'Prova!',      
+    }
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<template>
+    <div>
+        <img src="../assets/gen-v-il-teaser-trailer-dello-spin-off-di-the-boys.jpg" alt="">
+        <img src="/public/genV.webp" alt="">
+    </div>
+ 
+ 
+</template>
+
+<style scoped>
+</style>
